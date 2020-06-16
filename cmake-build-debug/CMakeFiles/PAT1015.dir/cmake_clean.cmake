253e7b9c7dd2de103acdeb53b611d92916e851c0
duanyuxiang <duanyx@belink.com>
2020-06-16 15:40:37 +0800

    first commit

---
+++ cmake-build-debug/CMakeFiles/PAT1015.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PAT1015.dir/main.c.obj"
+  "PAT1015.exe"
+  "PAT1015.exe.manifest"
+  "PAT1015.pdb"
+  "libPAT1015.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/PAT1015.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+    printf("qwe");
+    return 0;
+}
